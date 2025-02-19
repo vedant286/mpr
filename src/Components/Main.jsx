f65862036d0c64f12133d1070f2f881427277792
vedantk <vedantkadam286@gmail.com>
2025-02-19 19:44:38 +0530

    new point

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,13 +1,18 @@
-import { } from 'fontawesome'
-
+import { Link } from "react-router";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse,faMagnifyingGlass,faMessage,faUser,faGear } from "@fortawesome/free-solid-svg-icons";
 const Main = () => {
 
     return (
         <>
             <div className="w-full h-[100vh]">
-                <div className="bg-black w-20 h-full flex">
-                    <h1 className="font-bold text-[#FEC461] text-4xl mx-auto mt-5">N</h1>
-
+                <div className="bg-black w-20 h-full flex flex-col justify-evenly">
+                    <h1 className="font-bold text-[#FEC461] text-4xl mx-auto mt-0"><Link to="/">N</Link></h1>
+                    <FontAwesomeIcon icon={faHouse} color="#FEC461" size="2x"/>
+                    <FontAwesomeIcon icon={faMagnifyingGlass} color="#FEC461" size="2x"/>
+                    <FontAwesomeIcon icon={faMessage} color="#FEC461" size="2x"/>
+                    <FontAwesomeIcon icon={faUser} color="#FEC461" size="2x"/>
+                    <FontAwesomeIcon icon={faGear} color="#FEC461" size="2x"/>
                 </div>
                     
             </div>
